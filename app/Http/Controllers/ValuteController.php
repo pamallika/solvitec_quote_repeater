58d4b204d98643540239d24189c4c187613c04f6
Alexandr Fedorenko <sasha.fedorenko4736@gmail.com>
2023-11-10 02:13:48 +0300

    fix payload sbr

--- app/Http/Controllers/ValuteController.php
+++ app/Http/Controllers/ValuteController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\CbrAdapter;
 use App\Models\Valute;
+use Carbon\Exceptions\InvalidFormatException;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Mockery\Exception;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use function App\Helpers\errResponse;
 use function App\Helpers\okResponse;
@@ -20,9 +22,13 @@ class ValuteController extends Controller
         if (!$date = $request->get('date_req')) {
             return errResponse('Param date undefined', ResponseAlias::HTTP_NOT_FOUND);
         }
+        try {
+            $dateCarbon = Carbon::createFromFormat('d/m/Y', $date);
+        } catch (InvalidFormatException $e) {
+            return errResponse('date_req format must be ' . 'd/m/Y ' . $e->getMessage(), ResponseAlias::HTTP_UNPROCESSABLE_ENTITY);
+        }
 
-        $dateCarbon = Carbon::createFromFormat('d/m/Y', $date);
-        $formatedDate = $dateCarbon->format('Y-m-d');
+        $formatedDate = $dateCarbon->format('Y - m - d');
 
         /** @var Valute $valute */
         if ($dateCarbon->isPast() && $valute = Valute::where('date', $formatedDate)->first()) {
@@ -30,7 +36,7 @@ class ValuteController extends Controller
         }
 
         $client = new Client(['base_uri' => self::CBR_BASE_URI]);
-        $response = $client->request('GET', '/scripts/XML_daily.asp', ['query' => ['date_req' => $date]]);
+        $response = $client->request('GET', ' / scripts / XML_daily . asp', ['query' => ['date_req' => $date]]);
         if ($response->getStatusCode() !== ResponseAlias::HTTP_OK) {
             return errResponse('CBR server is not responding, try again later');
         }
